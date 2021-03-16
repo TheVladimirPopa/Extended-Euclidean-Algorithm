655b52e6520ebea83d8cfe1174977a41e2776304
TheVladimirPopa <66223401+TheVladimirPopa@users.noreply.github.com>
2021-03-16 11:31:02 +0200

    Added list format and gcd calculation.

--- Main.py
+++ Main.py
@@ -1,31 +1,44 @@
-def genArray(size):
-    arr = [0 for x in range(size)]
+def generate_array(length):
+    arr = [0 for x in range(length)]
     return arr
 
 
-def genMatrix(w, h):
+def generate_matrix(w, h):
     mat = [[0 for x in range(w)] for y in range(h)]
     return mat
 
 
+def print_stuff(collection, s):
+    for t in range(0, s + 1):
+        print("%d " % collection[t], end='')
+
+
 if __name__ == '__main__':
-    size = 6
+    size = 6  # chosen at random
 
     a = input("Input a:")
     a = int(a)
     b = input("Input b:")
     b = int(b)
+    print()
+
+    if a > b:
+        c = a
+        a = b
+        b = c
+        print("(Note: a and b have been swapped because a < b)")
+        print()
 
     alpha = 0
     beta = 1
     i = 0
 
-    k = genArray(size)
+    k = generate_array(size)
 
-    formatA = genArray(size)
-    formatB = genArray(size)
+    formatA = generate_array(size)
+    formatB = generate_array(size)
 
-    formatST = genMatrix(2, size)
+    formatST = generate_matrix(2, size)
 
     formatA[0] = a
     formatB[0] = b
@@ -48,7 +61,40 @@ if __name__ == '__main__':
         formatST[i - j - 1][0] = alpha
         formatST[i - j - 1][1] = beta
 
-    print("a b k alpha beta")
+    print("As table:")
+    print("a\tb\tk\talpha\tbeta")
 
     for j in range(0, i + 1):
         print("%d  %d  %d  %d  %d" % (formatA[j], formatB[j], k[j], formatST[j][0], formatST[j][1]))
+
+    print()
+
+    print("Greatest common divisor(%d,%d) = %d * %d + %d * %d = %d" % (formatA[0], formatB[0],
+                                                                       formatA[0], formatST[0][0], formatB[0],
+                                                                       formatST[0][1],
+                                                                       formatA[0] * formatST[0][0] + formatB[0] *
+                                                                       formatST[0][1]))
+
+    print()
+    print("As lists:")
+    print("a: ", end='')
+    print_stuff(formatA, i)
+
+    print()
+    print("b: ", end='')
+    print_stuff(formatB, i)
+
+    print()
+    print("k: ", end='')
+    print_stuff(k, i)
+
+    print()
+    print("alpha: ", end='')
+    for j in range(0, i + 1):
+        print("%d " % formatST[j][0], end='')
+
+    print()
+    print("beta: ", end='')
+    for j in range(0, i + 1):
+        print("%d " % formatST[j][1], end='')
+    print()
